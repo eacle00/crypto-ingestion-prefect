58cbb0ff22e3ef2da2182605f3cceb7b118a72a6
eacle00 <allenspencerr@gmail.com>
2025-08-11 10:47:51 +0800

    Modified app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from prefect import flow, task
+import requests
+from datetime import datetime
 
 @task
-def say_hello(name: str):
-    print(f"Hello, {name}!")
+def fetch_btc():
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    params = {
+        "ids": "bitcoin",
+        "vs_currencies": "usd"
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+    price = data["bitcoin"]["usd"]
+    return price
 
+@task
+def load_btc(price: float):
+    timestamp = datetime.now().isoformat()
+    
 @flow
-def hello_flow():
-    say_hello("Prefect Cloud")
+def ingestion_flow():
+    price = fetch_btc()
+    load_btc(price)
 
 if __name__ == "__main__":
     hello_flow()
