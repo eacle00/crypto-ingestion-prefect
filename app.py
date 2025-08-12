762dea259793222d72473175b5c7b137be7e226e
eacle00 <allenspencerr@gmail.com>
2025-08-12 11:03:09 +0800

    Fix issue with prefect and bigquery

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from prefect import flow, task
 from datetime import datetime
-from prefect_gcp import GcpCredentials
-#from pandas_gbq import to_gbq
-from prefect_gcp.bigquery import BigQueryWarehouse
+from prefect_gcp.secret_manager import GcpSecret
 import pandas as pd
 import json
 import requests
@@ -22,50 +20,31 @@ def fetch_btc():
 
     return price
 
-# @task
-# def create_dataframe(price: float):
-#     timestamp = datetime.now().isoformat()
-#     data = {
-#         "Timestamp":[timestamp],
-#         "BTC_USD":[price]
-#     }
+@task
+def create_dataframe(price: float):
+    timestamp = datetime.now().isoformat()
+    data = {
+        "Timestamp":[timestamp],
+        "BTC_USD":[price]
+    }
 
-#     return pd.DataFrame(data)
+    return pd.DataFrame(data)
 
 @task
-def load_to_bq(price: float):
-    # gcp_credentials_block = GcpCredentials.load("my-gcp-creds")
-    # print(gcp_credentials_block.project)
-    # print(gcp_credentials_block.get_credentials_from_service_account())
-    # to_gbq(
-    #     dataframe = df,
-    #     destination_table = 'crypto_ingestion_0.btc_prices',
-    #     project_id = 'crypto-ingestion-468703',
-    #     credentials = gcp_credentials_block.get_credentials_from_service_account(),
-    #     if_exists = 'append'
-    # )
-    # print("Data uploaded to BigQuery successfully!")
-    timestamp = datetime.now().isoformat()
-    bigquery_warehouse_block = BigQueryWarehouse.load("bgq-wh")
-    create_sql = """
-    CREATE TABLE IF NOT EXISTS `crypto_ingestion_0.btc_prices` (
-        Timestamp TIMESTAMP,
-        BTC_USD FLOAT
+def load_to_bq(df: pd.DataFrame):
+    gcp_credentials_block = GcpCredentials.load("gcp-bgq-creds")
+    df.to_gbq(
+        destination_table = 'crypto_coins.btc_prices',
+        project_id = 'crypto-ingestion',
+        credentials = gcp_credentials_block.get_credentials_from_service_account(),
+        if_exists = 'append'
     )
-    """
-    insert_sql = f"""
-    INSERT INTO `crypto_ingestion_0.btc_prices` (Timestamp, BTC_USD)
-    VALUES ({timestamp},{price})
-    """
     
-    bigquery_warehouse_block.execute(create_sql)
-    bigquery_warehouse_block.execute(insert_sql)
-
 
 @flow(log_prints=True)
 def ingestion_flow():
     price = fetch_btc()
-    # df = create_dataframe(price)
+    df = create_dataframe(price)
     load_to_bq(price)
 
 
